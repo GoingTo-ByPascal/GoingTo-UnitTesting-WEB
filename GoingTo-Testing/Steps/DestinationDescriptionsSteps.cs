95f09da4bddf711bb92623dae1eca82524395ba0
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-10-19 18:42:41 -0500

    Se implementa testing para obtener las descripciones de lugares

---
+++ GoingTo-Testing/Steps/DestinationDescriptionsSteps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace GoingTo_Testing.Steps
+{
+    [Binding]
+    public class DestinationDescriptionsSteps
+    {
+        private RestClient client;
+        private RestRequest request;
+        IRestResponse response;
+
+        private ScenarioContext _scenarioContext;
+
+        public DestinationDescriptionsSteps(ScenarioContext scenarioContext) 
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I'm looking for a description of a destination")]
+        public void GivenIMLookingForADescriptionOfADestination()
+        {
+            client = new RestClient("https://goingto.azurewebsites.net/api");
+            request = new RestRequest("/locatable/{locatableid}", Method.GET, DataFormat.Json);
+            request.AddUrlSegment("locatableid", 1);
+        }
+        
+        [When(@"I  execute the request")]
+        public void WhenIExecuteTheRequest()
+        {
+            response = client.Execute(request);
+        }
+        
+        [Then(@"the  result should be (.*)")]
+        public void ThenTheResultShouldBe(int status)
+        {
+            int statusCode = (int)response.StatusCode;
+            Assert.AreEqual(status, statusCode, "Status code is not 200");
+        }
+    }
+}
