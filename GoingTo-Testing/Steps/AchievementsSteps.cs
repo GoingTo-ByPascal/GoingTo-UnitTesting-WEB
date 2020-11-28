c3b101152a63dbd7d53b68d0a8acbeb3fc3e26fe
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-11-27 22:41:23 -0500

    Actualización del client request

--- GoingTo-Testing/Steps/AchievementsSteps.cs
+++ GoingTo-Testing/Steps/AchievementsSteps.cs
@@ -11,27 +11,31 @@ namespace GoingTo_Testing.Feature
         private RestClient client;
         private RestRequest request;
         IRestResponse response;
-
         private ScenarioContext _scenarioContext;
+
+        public AchievementsSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
 
         [Given(@"I search for systems's achievements")]
         public void GivenISearchForSystemsSAchievements()
         {
             request = new RestRequest("/achievements", Method.GET, DataFormat.Json);
-            client = new RestClient("https://goingto.azurewebsites.net/api");
+            client = new RestClient("https://api-goingto.azurewebsites.net/api");
         }
-        
         [When(@"I interact with the system")]
         public void WhenIInteractWithTheSystem()
         {
             response = client.Execute(request);
         }
-        
-        [Then(@"the result of the operation should be (.*)")]
-        public void ThenTheResultOfTheOperationShouldBe(int status)
-        {
-            int statusCode = (int)response.StatusCode;
-            Assert.AreEqual(status, statusCode, "Status code is not 200");
+
+            [Then(@"the result of the operation should be (.*)")]
+            public void ThenTheResultOfTheOperationShouldBe(int status)
+            {
+                int statusCode = (int)response.StatusCode;
+                Assert.AreEqual(status, statusCode, "Status code is not 200");
+            }
         }
-    }
-}
+    } 
+
